98367d443981c52c52fe7e07ec9b5030ff9818b4
Radley <radleygh@gmail.com>
2017-09-28 22:16:14 -0700

    Added post thumbnails and pinterest share links to destinations

--- _template-parts/flexible-fields/destinations.php
+++ _template-parts/flexible-fields/destinations.php
@@ -24,13 +24,30 @@ if ( $dest_query->have_posts() ) {
 		while( $dest_query->have_posts() ): $dest_query->the_post();
 			$date_range = aa_destination_date_range();
 			?>
-			<div class="destination-item columns one-third <?php post_class(); ?>">
+			<div <?php post_class('destination-item columns one-third'); ?>>
 				
-				<?php if ( has_post_thumbnail() ) { ?>
-				<div class="image">
-					<?php the_post_thumbnail( get_the_ID(), 'thumbnail' ); ?>
-				</div>
-				<?php } ?>
+				<?php
+				if ( has_post_thumbnail() ) {
+					$image_id = get_post_thumbnail_id();
+					$image_full = wp_get_attachment_image_src( $image_id, 'full' );
+					$image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+					if ( !$image_alt ) $image_alt = get_the_title( $image_id );
+					
+					$pinit_url = add_query_arg( array(
+						'url' => urlencode( get_permalink() ),
+					    'media' => urlencode( $image_full[0] ),
+					    'description' => urlencode( $image_alt )
+					), 'https://pinterest.com/pin/create/button/' );
+					?>
+					<div class="image">
+						<a href="<?php echo esc_attr($pinit_url); ?>" class="pin-it" target="_blank" rel="external">
+							<?php the_post_thumbnail( get_the_ID(), 'thumbnail' ); ?>
+							<span class="pin-it-label">Pin It</span>
+						</a>
+					</div>
+					<?php
+				}
+				?>
 				
 				<h4 class="title"><a href="<?php echo esc_attr(get_permalink()); ?>" title="Read more about <?php echo esc_attr(get_the_title()); ?>"><?php echo esc_html(get_the_title()); ?></a></h4>
 				
