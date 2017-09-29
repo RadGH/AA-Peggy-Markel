4e353008f8728abd44eb730da3559b0ea856f139
Radley <radleygh@gmail.com>
2017-09-29 03:11:28 -0700

    Finished testimonials setup. Added flickity slider (for testimonials)

--- _template-parts/flexible-fields/testimonials.php
+++ _template-parts/flexible-fields/testimonials.php
@@ -10,4 +10,22 @@ if ( !isset($flex_field) ) return; // This variable is passed through from "flex
 aa_flexible_field_title( $flex_field );
 aa_flexible_field_subtitle( $flex_field );
 
-echo 'TODO: testimonials';
+$testimonials = !empty($flex_field['testimonial_list']) ? $flex_field['testimonial_list'] : false;
+if ( $testimonials ) {
+	?>
+	<div class="testimonial-list" data-flickity='{ "wrapAround": true, "prevNextButtons": false, "pageDots": true, "autoPlay": 5000 }'>
+		<?php
+		foreach( $testimonials as $t ) {
+			$message = '&ldquo;' . $t['message'] . '&rdquo;';
+			$author = $t['author'];
+			?>
+			<div class="testimonial-item">
+				<div class="message"><?php echo wpautop($message); ?></div>
+				<div class="author">&ndash; <?php echo esc_html($author); ?></div>
+			</div>
+			<?php
+		}
+		?>
+	</div>
+	<?php
+}
