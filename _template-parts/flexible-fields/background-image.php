12640edb12eea33280ca48d469b4accddc9eab6d
Radley <radleygh@gmail.com>
2017-09-28 21:34:43 -0700

    Added separate background image field for more obvious usage on the backend

---
+++ _template-parts/flexible-fields/background-image.php
@@ -0,0 +1,11 @@
+<?php
+/* Flexible field: Text
+ *
+ * Displays title, subtitle, a block of text, and a button. All of these are optional.
+ */
+
+if ( !isset($flex_field) ) return; // This variable is passed through from "flexible-content-areas.php"
+
+
+// This section does not have any content to display.
+echo '&nbsp;';
