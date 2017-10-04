cd6027a2261d3c8918d9a70fb3758368ca37f5c3
Radley <radleygh@gmail.com>
2017-10-03 20:39:34 -0700

    Removed contextual help and example shortcode

--- _includes/custom-post-types/destinations.php
+++ _includes/custom-post-types/destinations.php
@@ -56,57 +56,6 @@ function aa_register_destinations_post_type() {
 }
 add_action( 'init', 'aa_register_destinations_post_type' );
 
-/*-----------------------------------------------------------------------------------*/
-/*  CONTEXTUAL HELP (optional)
-/*-----------------------------------------------------------------------------------*/
-
-function aa_destination_contextual_help( $contextual_help, $screen_id, $screen ) {
-	if ( 'edit-videos' == $screen->id ) {
-		
-		$contextual_help = '<h2>Videos</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est libero, congue eget gravida mollis, eleifend sit amet felis.</p> 
-    <p>Sed mollis pretium dolor at vestibulum. Phasellus condimentum dui in velit interdum, sed aliquam ex hendrerit.</p>';
-		
-	} elseif ( 'videos' == $screen->id ) {
-		
-		$contextual_help = '<h2>Editing Videos</h2>
-    <p>Nunc eleifend arcu sit amet tortor <strong>luctus aliquam.</strong> Sed a massa vestibulum, iaculis magna ac, faucibus tellus.</p>';
-		
-	}
-	return $contextual_help;
-}
-add_action( 'contextual_help', 'aa_destination_contextual_help', 10, 3 );
-
-/*---------------------------------------------------------------
-	LATEST POSTS FROM THIS CUSTOM POST TYPE SHORTCODE (optional)
-------------------------------------------------------------------*/
-
-function latest_videos_shortcode($atts, $content){
-	$html = '<div class="videos"><ul>';
-	
-	global $post;
-	
-	$args = array( 'posts_per_page' => $atts['no'],
-	               'offset'=> 0,
-	               'post_type' => 'videos');
-	$all_posts = new WP_Query($args);
-	
-	while($all_posts->have_posts()) : $all_posts->the_post();
-		
-		$html .= '<li><a href="' . get_permalink() . '">
-			<div class="caption">
-				<h3>' . get_the_title() . '</h3>
-			</div>' . get_the_post_thumbnail($post->ID, 'thumb-video') . '
-		</a></li>';
-	
-	endwhile;
-	
-	$html .= '</ul></div>';
-	return $html;
-}
-add_shortcode('latest_videos', 'latest_videos_shortcode');
-
-
 /**
  * Return the date range in a string format, eg: August 7 - 15, 2016
  */
