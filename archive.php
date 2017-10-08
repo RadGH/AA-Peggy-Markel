c2960a07ec584d7bd7316b99af6afa0297e3e67c
Radley <radleygh@gmail.com>
2017-10-08 15:35:51 -0700

    Adjusted blog page headers and clear filter button

--- archive.php
+++ archive.php
@@ -8,13 +8,6 @@ if ( !have_posts() ) {
 	return;
 }
 
-$archive_title = '';
-if ( is_category() ) $archive_title = single_term_title( '', false );
-else if ( is_tag() ) $archive_title = single_term_title( '', false );
-else if ( is_date() ) $archive_title = get_the_time('F Y');
-else if ( is_month() ) $archive_title = get_the_time('F Y');
-else if ( is_year() ) $archive_title = get_the_time('Y');
-
 get_header();
 
 // Blog post header
@@ -29,16 +22,6 @@ if ( !is_search() && (is_post_type_archive('post') || is_home() || is_category()
 	
 	<article>
 		<div class="content-area">
-			
-			<?php if ( $archive_title ) { ?>
-			<div class="archive-intro">
-				<h1 class="archive-title h3"><?php echo esc_html($archive_title); ?> <a href="<?php echo esc_attr(get_post_type_archive_link('post')); ?>" class="clear-filter">Clear filter</a></h1>
-				
-				<?php if ( is_category() || is_tag() ) { ?>
-					<div class="archive-content"><?php echo category_description( get_queried_object_id() ); ?></div>
-				<?php } ?>
-			</div>
-			<?php } ?>
 			
 			
 			<?php
