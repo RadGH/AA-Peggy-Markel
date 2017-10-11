8816598675c4a39036b20525578cfb06022c14a3
Radley <radleygh@gmail.com>
2017-10-11 03:55:28 -0700

    Finished store

--- _includes/extended/woocommerce.php
+++ _includes/extended/woocommerce.php
@@ -5,15 +5,47 @@ if ( !class_exists('WooCommerce') ) return;
 
 add_theme_support( 'woocommerce' );
 
-function aa_wc_reorganize_woocommerce_hooks() {
-	// Disable WooCommerce "On Sale" div
-	// add_filter( 'woocommerce_sale_flash', '__return_false' );
+// Remove the description tab
+function aa_remove_description_tab( $tabs ) {
+	if ( isset($tabs['description']) ) unset($tabs['description']);
+	return $tabs;
+}
+
+// Add the description
+function aa_insert_product_description_below_title() {
+	$heading = esc_html( apply_filters( 'woocommerce_product_description_heading', __( '', 'woocommerce' ) ) );
+	?>
 	
-	// Disable default placement of product page "Sort by" dropdown (see templates/parts/store-banner.php)
-	// remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+	<div class="product-description">
+		<?php if ( $heading ): ?>
+			<h2><?php echo $heading; ?></h2>
+		<?php endif; ?>
+		
+		<?php the_content(); ?>
+	</div>
+	
+	<?php
+}
+
+function aa_wc_reorganize_woocommerce_hooks() {
+	// Remove product description tab, move description below title instead
+	add_filter( 'woocommerce_product_tabs', 'aa_remove_description_tab' );
+	add_action( 'woocommerce_single_product_summary', 'aa_insert_product_description_below_title', 7 );
+	
+	// Remove add to cart button on archive
+	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+	
+	// Disable the breadcrumbs
+	remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
+	
+	// Disable WooCommerce "On Sale" div
+	add_filter( 'woocommerce_sale_flash', '__return_false' );
+	
+	// Disable "Sort by" dropdown
+	remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
 	
 	// Disable the line of text that read "Showing all xxx results"
-	// remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+	remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
 	
 	// Move SKU below title on single products
 	// @add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
@@ -64,6 +96,10 @@ add_filter( 'body_class', 'aa_wc_woocommerce_body_class_for_pages' );
 
 // Customize the WooCommerce template using hooks
 function aa_wc_woocommerce_template_hooks() {
+	// Add underline to single product titles
+	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+	add_action('woocommerce_single_product_summary', 'aa_single_product_title', 5 );
+	
 	// Display markup before woocommerce
 	remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 	add_action('woocommerce_before_main_content', 'aa_wc_woocommerce_before', 10 );
@@ -87,7 +123,7 @@ add_action( 'wp', 'aa_wc_woocommerce_template_hooks' );
 // Hooks used before and after WooCommerce template content
 function aa_wc_woocommerce_before() {
 	
-	if ( !is_cart() && !is_checkout() ) {
+	if ( !is_cart() && !is_checkout() && !is_singular('product') ) {
 		get_template_part( '_template-parts/header', 'shop' );
 	}
 	
@@ -119,3 +155,15 @@ function aa_wc_disable_order_notes( $fields ) {
 	return $fields;
 }
 add_filter( 'woocommerce_checkout_fields' , 'aa_wc_disable_order_notes' );
+
+// Disable page title on shop archive
+function aa_disable_shop_archive_title( $show ) {
+	if ( is_shop() && is_archive() ) return false;
+	return $show;
+}
+add_filter( 'woocommerce_show_page_title', 'aa_disable_shop_archive_title' );
+
+// Add an underline to singular product titles
+function aa_single_product_title() {
+	the_title( '<h1 itemprop="name" class="product_title entry-title"><span class="pm-underline">', '</span></h1>' );
+}
