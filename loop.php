5ad9d9afc20e81045314fdc7616ca69dbc3051af
Radley <radleygh@gmail.com>
2017-10-14 02:50:00 -0700

    Tidied up page templates for single pages and destinations

--- loop.php
+++ loop.php
@@ -1,41 +1,13 @@
-<?php
-/**
- * The loop
- */
-
-global $more
-?>
-
-<?php //If there are no posts to display, such as an empty archive page
-	if ( ! have_posts() ) : ?>
-		<h1>There are currently no entries for this archive.</h1>
-		<?php get_search_form(); ?>
-<?php endif; ?>
-
-<?php while ( have_posts() ) : the_post(); ?>
-
-	<!-- Display All Posts -->
-		<div id="post-<?php the_ID(); ?>" <?php post_class('post clearfix'); ?>>
-			<?php 
-				the_post_thumbnail('blog-posts'); ?>
-<div class="post-preview">
-  <h4 class="post-title"><a href="<?php the_permalink(); ?>">
-    <?php the_title(); ?>
-    </a></h4>
-    <div class="post-meta">by <a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>">
-      <?php the_author(); ?>
-      </a> under <?php the_category(' , '); ?>&ensp;|&ensp; <span class="post-date">
-      <?php the_time('F j, Y'); ?>
-      </span> &ensp;|&ensp;
-      <?php comments_number(__('No Comments')); ?>
-    </div>
-  <?php the_excerpt(); ?>
-  <?php if(has_tag()) : ?><div class="tags"><?php the_tags('Tags: ', ', '); ?></div><?php endif; ?>
-  <div class="read-more"><a href="<?php the_permalink(); ?>" class="button">read more</a></div>
-</div>
-		</div>
+<article>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header>
+	<!-- .entry-header -->
 	
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+	<!-- /.entry-content -->
 
-<?php endwhile;
-wp_reset_query();
-?>
+</article>
+<!-- /#post-## -->
--- single.php
+++ single.php
@@ -9,15 +9,41 @@ if ( !have_posts() ) {
 }
 
 get_header();
+
+// Hero section
+get_template_part( '_template-parts/hero-section', 'single' );
 ?>
 
 <div class="container">
     <?php
+
     while ( have_posts() ) : the_post();
-
-	    get_template_part( 'loop', get_post_type() );
-
-    endwhile; // End of the loop.
+	
+	    ?>
+	    <article>
+		    
+		    <?php
+		    // Title can be replaced by a hero section
+		    if ( empty(get_field( 'hero_toggle', $hero_object_id )) ) {
+		    	?>
+			    <header class="entry-header">
+				    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			    </header>
+			    <!-- .entry-header -->
+			    <?php
+		    }
+		    ?>
+		
+		    <div class="entry-content">
+			    <?php the_content(); ?>
+		    </div>
+		    <!-- /.entry-content -->
+	
+	    </article>
+	    <!-- /#post-## -->
+	    <?php
+	
+	    endwhile; // End of the loop.
     ?>
 </div> <!-- /.container -->
 
