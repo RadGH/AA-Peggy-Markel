8838d99b802fa758c2b2f915f8c74e23f49c284c
Radley <radleygh@gmail.com>
2017-10-20 22:37:10 -0700

    Full page search

--- search.php
+++ search.php
@@ -13,33 +13,29 @@ new WP_Widget_Search();
 ?>
 <div class="container">
 	
-	<?php get_sidebar(); ?>
-	
 	<article>
-		<div class="content-area">
-			<?php if ( have_posts() && $s ) : ?>
-				
-				<div class="archive-intro search-intro">
-					<h1 class="heading">Search</h1>
-					<p>Viewing results for &ldquo;<?php echo esc_html( $s ); ?>&rdquo;</p>
-					
-					<?php get_search_form(); ?>
-				</div>
-				
-				<?php get_template_part( 'loop', 'search' ); ?>
-				
-				<?php get_template_part( '_template-parts/part', 'navigation' ); ?>
+		<?php if ( have_posts() && $s ) : ?>
 			
-			<?php else : ?>
+			<div class="archive-intro search-intro">
+				<h1 class="heading">Search</h1>
+				<p>Viewing results for &ldquo;<?php echo esc_html( $s ); ?>&rdquo;</p>
+				
+				<?php get_search_form(); ?>
+			</div>
 			
-				<div class="archive-intro search-intro">
-					<h1 class="heading">Search</h1>
-					<p>Enter search keywords below to begin.</p>
-					<?php get_search_form(); ?>
-				</div>
+			<?php get_template_part( 'loop', 'search' ); ?>
 			
-			<?php endif; ?>
-		</div>
+			<?php get_template_part( '_template-parts/part', 'navigation' ); ?>
+		
+		<?php else : ?>
+		
+			<div class="archive-intro search-intro">
+				<h1 class="heading">Search</h1>
+				<p>Enter search keywords below to begin.</p>
+				<?php get_search_form(); ?>
+			</div>
+		
+		<?php endif; ?>
 	</article>
 	
 </div>
